78e655580bfd165b8236e69904d4671d84af49e4
ruannvdw <ruannvdw@gmail.com>
2018-08-10 14:44:13 +0200

    Continuing Development

--- BlackJackGame.py
+++ BlackJackGame.py
@@ -56,11 +56,15 @@ class Hand():
         self.new_card = play_deck.deal()
         if self.new_card[1] == 'Ace ':
             ace_value = None
-            while ace_value != 1 and ace_value != 11 and type(ace_value) == int:
-                ace_value = int(input('Choose 1 or 11:\n'))
-                self.value += ace_value
-                break
-
+            while True:
+                try:
+                    while ace_value != 1 and ace_value != 11:
+                        ace_value = int(input('Choose 1 or 11:\n'))
+                        self.value += ace_value
+                        break
+                    break
+                except:
+                    print('Enter a valid integer')
         else:
             self.value += values[str(self.new_card[1])]
 
--- Testing.py
+++ Testing.py
@@ -1,3 +1,8 @@
 dict = {'one': 1, 'two': 2}
 
 print(dict['one'])
+
+while ace_value != 1 and ace_value != 11 and type(ace_value) != int:
+    ace_value = int(input('Choose 1 or 11:\n'))
+    self.value += ace_value
+    break
